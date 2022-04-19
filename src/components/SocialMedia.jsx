8413daec9d2528939c39c03430960c28ba8a5d79
blessed-tawanda <blessedmahuni@gmail.com>
2022-04-20 00:49:55 +0200

    self branding mising picture and logo

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import { BsTwitter, BsInstagram } from "react-icons/bs";
+import { BsTwitter, BsLinkedin } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
        <div>
-         <BsTwitter></BsTwitter>
+         <a href="https://twitter.com/@WoodyJohnsonMah" target={"_blank"}><BsTwitter/></a>
        </div>
        <div>
-         <FaFacebookF></FaFacebookF>
+        <a href="https://facebook.com/tawanda.187" target={"_blank"}><FaFacebookF/></a>
        </div>
        <div>
-         <BsInstagram></BsInstagram>
+        <a href="https://www.linkedin.com/in/blessed-tawanda-mahuni" target={"_blank"}><BsLinkedin/></a>
        </div>
     </div>
   )
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -45,7 +45,7 @@ const Header = () => {
         transition={{duration: 0.5, delayChildren: 0.5}}
         className="app__header-img"
       >
-        <img src={images.profile} alt="profile_bg" />
+        {/* <img src="" alt="profile_bg" /> */}
         <motion.img
           whileInView={{ scal: [0, 1] }}
           transition={{duration: 1, ease: 'easeInOut'}}
@@ -59,7 +59,7 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+        {[images.react, images.node, images.typescript].map((circle, index) => (
           <div className="circle-cmp app__flex" key={`circle-${index}`}>
             <img src={circle} alt="circle" />
           </div>
