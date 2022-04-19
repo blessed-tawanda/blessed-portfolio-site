79849636ac5e8da3cf1d31bf310618877a85b799
blessed-tawanda <blessedmahuni@gmail.com>
2022-04-20 01:33:07 +0200

    configure env files to work in netlify

--- src/client.js
+++ src/client.js
@@ -1,12 +1,13 @@
 import SanityClient  from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
+import env from 'react-dotenv';
 
 export const client = SanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2022-02-27',
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN
+  token: env.REACT_APP_SANITY_TOKEN
 })
 
 const builder = imageUrlBuilder(client);
