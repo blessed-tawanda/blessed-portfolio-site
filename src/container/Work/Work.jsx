890983ee8577e98ff5fde900dca5f4eea5fbed29
blessed-tawanda <blessedmahuni@gmail.com>
2022-06-07 15:42:00 +0200

    Work in progress

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -43,7 +43,7 @@ const Work = () => {
    <>
     <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
     <div className="app__work-filter">
-      {['UI/UX', 'Web App', 'Mobile App', 'Resct JS', 'All'].map((item, index) => (
+      {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'NodeJS' ,'All'].map((item, index) => (
         <div
           key={index}
           onClick={() => handleWorkFilter(item)}
